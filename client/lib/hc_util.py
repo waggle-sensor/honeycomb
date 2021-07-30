78d66a2bacc3a8e1a97b0aa854bc2a1c109e04ec
Kenan <karica2@uic.edu>
2021-07-30 10:01:26 -0500

    Changed /queue-upgrade to just take a file path

--- client/lib/hc_util.py
+++ client/lib/hc_util.py
@@ -7,132 +7,6 @@ import hashlib
 import shutil
 
 
-def init_upgrade_env(payload):
-    logging.info("Creating upgrade environment")
-
-    # look for upgrade file based on where the payload says it is
-    if not os.path.isfile(f"./upgrades/{payload['config_file']}"):
-        return {
-            "message": f"payload file ./upgrades/{payload['config_file']} not found\n",
-            "return_code": 400,
-        }
-
-    # checking to see we have a .tar or .tar.gz
-    if not payload["config_file"].endswith(".tar") and not payload[
-        "config_file"
-    ].endswith(".tar.gz"):
-
-        return {
-            "message": f"payload file {payload['config_file']} is not a .tar or .tar.gz file\n",
-            "return_code": 400,
-        }
-
-    # upgrade env has already been created?
-
-    upgrade_name = f"{payload['peripheral_name']}-{payload['config_version']}"
-    if os.path.isdir(f"./jobs/{upgrade_name}"):
-
-        return {
-            "message": f"job directory {upgrade_name} already exists\n",
-            "return_code": 400,
-        }
-
-    config_file = tarfile.open(f"./upgrades/{payload['config_file']}", "r:*")
-    os.mkdir(f"./jobs/{upgrade_name}")
-    # Try to extract the tarfile
-    try:
-        config_file.extractall(path=f"./jobs/{upgrade_name}")
-    except tarfile.ReadError:
-        return {
-            "message": f"ReadError: Could not extract {payload['config_file']}\n",
-            "return_code": 400,
-        }
-
-    config_file.close()
-
-    # if all is good, return it as so
-    return {"message": "All good\n", "return_code": 200}
-
-
-def check_payload_validity(payload):
-
-    # check for a set of keys in our payload that we really need
-    fields = ["node_id", "flags", "peripheral_name", "config_version", "config_file"]
-
-    for field in fields:
-        if field not in payload:
-            return {
-                "message": f"{field} was not found in payload\n",
-                "return_code": 400,
-            }
-
-    if os.path.exists("/etc/waggle/node-id"):
-        with open("/etc/waggle/node-id") as nodeIDFile:
-            nodeID = nodeIDFile.readline()
-            if nodeID != payload["node_id"]:
-
-                return {
-                    "message": f"local nodeID {nodeID} does not match payload nodeID {payload['node_id']}\n",
-                    "return_code": 400,
-                }
-    logging.info("Payload request is valid")
-
-    # If all is good, return ok
-    return {"message": "All good\n", "return_code": 200}
-
-
-def verify_checksum(payload):
-
-    upgrade_name = f"{payload['peripheral_name']}-{payload['config_version']}"
-
-    # open our md5sum.chk file and verify checksums
-    # TODO: should I add a try/except here?
-
-    """ Our hashfile will have lines in the form of:
-        MD5SUM  /path/to/file
-        The two spaces between the sum and the path are why we go from index 0 to 2 on lines 103-104
-    """
-    with open(f"./jobs/{upgrade_name}/md5sum.chk", "r") as hashfile:
-
-        for line in hashfile:
-
-            # get the path and the hash
-            current_hash = line.split(" ")[0]
-            current_file = line.split(" ")[2].replace("\n", "")
-            current_file = f"./jobs/{upgrade_name}/{current_file.replace('./', '')}"
-
-            """ 
-                because of the way that md5sum.chk is created, the hash will always differ
-                when the file is being written. it checksums itself, but then adds more data after,
-                making the checksum inaccurate.
-                If this is a big deal, you might have to change how the checksums are generated in
-                the ./upgrades/build_upgrade.sh file. I think it's fine, though
-            """
-            if current_file == f"./jobs/{upgrade_name}/md5sum.chk":
-                continue
-
-            # open each file in the checksum file and verify the checksum
-
-            with open(current_file, "rb") as tmpfile:
-
-                hash = hashlib.md5()
-                fcontent = tmpfile.read()
-                hash.update(fcontent)
-                file_hash = hash.hexdigest()
-
-                if file_hash != current_hash:
-
-                    # clean up our environment
-                    shutil.rmtree(f"./jobs/{upgrade_name}")
-                    return {
-                        "message": f"{current_file} DOES NOT MATCH THE HASH\n",
-                        "return_code": 400,
-                    }
-
-    # if all is good, return it as so
-    return {"message": "All good\n", "return_code": 200}
-
-
 def check_manifest_validity(manifest, upgrade_name):
 
     required_fields = [
@@ -173,4 +47,98 @@ def check_required_files(manifest, job_path):
                 "message": f"Could not find file {job_path}/{file}",
                 "return_code": 400,
             }
+    return {"message": "All good\n", "return_code": 200}
+
+
+def check_for_tarfile(fname):
+
+    fullname = f"./upgrades/{fname}.tar.gz"
+
+    if not os.path.isfile(fullname):
+        return {"message": f"Could not find file {fullname}", "return_code": 400}
+    else:
+        return {"message": f"File {fullname}", "return_code": 200}
+
+
+def init_upgrade_env_tar(fname):
+    logging.info("Creating upgrade environment")
+
+    # look for upgrade file based on where the payload says it is
+    # if not os.path.isfile(f"./upgrades/{payload['config_file']}"):
+    #     return {
+    #         "message": f"payload file ./upgrades/{payload['config_file']} not found\n",
+    #         "return_code": 400,
+    #     }
+
+    # upgrade env has already been created?
+    if os.path.isdir(f"./jobs/{fname}"):
+
+        return {
+            "message": f"job directory {fname} already exists\n",
+            "return_code": 400,
+        }
+
+    config_file = tarfile.open(f"./upgrades/{fname}.tar.gz", "r:*")
+    os.mkdir(f"./jobs/{fname}")
+    # Try to extract the tarfile
+    try:
+        config_file.extractall(path=f"./jobs/{fname}")
+    except tarfile.ReadError:
+        return {
+            "message": f"ReadError: Could not extract {fname}.tar.gz\n",
+            "return_code": 400,
+        }
+
+    config_file.close()
+
+    # if all is good, return it as so
+    return {"message": "All good\n", "return_code": 200}
+
+
+def verify_checksum_tar(fname):
+
+    # open our md5sum.chk file and verify checksums
+    # TODO: should I add a try/except here?
+
+    """Our hashfile will have lines in the form of:
+    MD5SUM  /path/to/file
+    The two spaces between the sum and the path are why we go from index 0 to 2 on lines 103-104
+    """
+    with open(f"./jobs/{fname}/md5sum.chk", "r") as hashfile:
+
+        for line in hashfile:
+
+            # get the path and the hash
+            current_hash = line.split(" ")[0]
+            current_file = line.split(" ")[2].replace("\n", "")
+            current_file = f"./jobs/{fname}/{current_file.replace('./', '')}"
+
+            """ 
+                because of the way that md5sum.chk is created, the hash will always differ
+                when the file is being written. it checksums itself, but then adds more data after,
+                making the checksum inaccurate.
+                If this is a big deal, you might have to change how the checksums are generated in
+                the ./upgrades/build_upgrade.sh file. I think it's fine, though
+            """
+            if current_file == f"./jobs/{fname}/md5sum.chk":
+                continue
+
+            # open each file in the checksum file and verify the checksum
+
+            with open(current_file, "rb") as tmpfile:
+
+                hash = hashlib.md5()
+                fcontent = tmpfile.read()
+                hash.update(fcontent)
+                file_hash = hash.hexdigest()
+
+                if file_hash != current_hash:
+
+                    # clean up our environment
+                    shutil.rmtree(f"./jobs/{fname}")
+                    return {
+                        "message": f"{current_file} DOES NOT MATCH THE HASH\n",
+                        "return_code": 400,
+                    }
+
     return {"message": "All good\n", "return_code": 200}
--- client/routes/queue_upgrade.py
+++ client/routes/queue_upgrade.py
@@ -2,63 +2,59 @@ from flask import Blueprint, render_template, session, abort, request
 from flask import current_app as app
 
 from hc_util import (
-    init_upgrade_env,
-    verify_checksum,
-    check_payload_validity,
     check_required_files,
     check_manifest_validity,
+    check_for_tarfile,
+    init_upgrade_env_tar,
+    verify_checksum_tar,
 )
 
 import os
 import json
 import logging
-import tarfile
 
 logging.basicConfig(level=logging.INFO)
 
 upgrade = Blueprint("upgrade", __name__)
 
 
-@upgrade.route("/queue-upgrade", methods=["POST"])
+@upgrade.route("/queue-upgrade", methods=["POST", "GET"])
 def process():
 
-    payload = request.json
-    # logging.info(payload)
+    # get the query args
+    # we need file_name
+    if request.args.get("filename") is None:
+        return "No filename query arg provided!", 400
+
+    fname = request.args.get("filename")
 
     # go through every check we have, if it errors out, return that message and code
 
-    # check that we have a valid payload
-    payload_validity = check_payload_validity(payload)
-    if payload_validity["return_code"] != 200:
-        return payload_validity["message"], payload_validity["return_code"]
+    is_tarfile = check_for_tarfile(fname)
+    if is_tarfile["return_code"] != 200:
+        return is_tarfile["message"], is_tarfile["return_code"]
 
-    # set up payload env
-    env_validity = init_upgrade_env(payload)
+    env_validity = init_upgrade_env_tar(fname)
     if env_validity["return_code"] != 200:
         return env_validity["message"], env_validity["return_code"]
 
-    # verify the checksum
-    checksum_validity = verify_checksum(payload)
+    checksum_validity = verify_checksum_tar(fname)
     if checksum_validity["return_code"] != 200:
         return checksum_validity["message"], checksum_validity["return_code"]
 
-    # at this point, we're in the ./job/jobX directory, so we need to cd back to /honeycomb
-
-    # grab our metadata JSON, pass it to the job
-    upgrade_name = f"{payload['peripheral_name']}-{payload['config_version']}"
-
-    with open(f"./jobs/{upgrade_name}/metadata.json") as manifest:
+    with open(f"./jobs/{fname}/metadata.json") as manifest:
         manifest_json = json.load(manifest)
 
-    check_required_files(manifest_json, f"./jobs/{upgrade_name}")
+    # TODO: check error codes here
+    are_files_present = check_required_files(manifest_json, f"./jobs/{fname}")
+    if are_files_present["return_code"] != 200:
+        return are_files_present["message"], are_files_present["return_code"]
 
-    check_manifest_validity(manifest_json, upgrade_name)
+    valid_manifest = check_manifest_validity(manifest_json, fname)
+    if valid_manifest["return_code"] != 200:
+        return valid_manifest["message"], valid_manifest["return_code"]
 
-    app.honeycomb.addJob(manifest_json, upgrade_name)
-    logging.info("Job has been added to queue")
+    app.honeycomb.addJob(manifest_json, fname)
 
     # TODO: Change this success message
-    return {"message": "Upgrade called successfully\n"}, 200
-
-
-# TODO: make a cleanup function if the payload is not good. basically just delete the upgrade folder
+    return {"message": "Job has been added to queue\n"}, 200
